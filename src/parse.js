93fa2fb1e383633ad845f8b0f13b780e555efc65
Prateek Thapa <prateekthapa26@gmail.com>
2019-07-15 09:40:05 +0545

    basic parse

--- src/parse.js
+++ src/parse.js
@@ -2,8 +2,36 @@ const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
 
-const parenthesize = () => {};
+const parenthesize = (tokens) => tokens;
 
-const parse = () => {};
+const parse = (tokens) => {
+  const token = pop(tokens);
+
+  if (token.type === 'Number') { 
+    return {
+      type: 'NumericLiteral',
+      value: token.value,
+    }
+  }
+
+  if (token.type === 'String') { 
+    return {
+      type: 'StringLiteral',
+      value: token.value,
+    }
+  }
+
+  if (token.type === 'Name') { 
+    return {
+      type: 'Identifier',
+      name: token.value,
+    }
+  }
+
+  console.log(tokens);
+  if (isOpeningParenthesis(token.type)) { 
+    
+  }
+};
 
 module.exports = { parse: tokens => parse(parenthesize(tokens)) };
